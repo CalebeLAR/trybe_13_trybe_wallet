442f192cc9d2818c6f5655dbb6aa4369f47ab7f6
CalebeLAR <ca6ele@gmail.com>
2022-10-31 14:04:01 -0400

    iniciando projeto

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,1 +1,13 @@
-// Coloque aqui suas actions
+// ACTIONS CREATORS
+export const ADD_USER = 'ADD_USER';
+export const MOUNT_WALLET = 'MOUNT_WALLET';
+
+export const addUser = (user) => ({
+  type: ADD_USER,
+  user,
+});
+
+export const mountWalet = (wallet) => ({
+  type: MOUNT_WALLET,
+  wallet,
+});
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,5 +1,29 @@
+import { combineReducers } from 'redux';
 // import user from './user';
 // import wallet from './wallet';
 
 // Configure os seus reducers.
 // ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
+
+const INITIAL_STATE = {
+  user: {
+    email: '',
+  },
+  wallet: {
+    currencies: [],
+    expenses: [],
+    editor: false,
+    idToEdit: 0,
+  },
+};
+
+const exampleReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  default:
+    return state;
+  }
+};
+
+const rootReducer = combineReducers({ exampleReducer });
+
+export default rootReducer;
