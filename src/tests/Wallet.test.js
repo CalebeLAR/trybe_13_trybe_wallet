559065e4206a97b1beedaaabce7dd4f0faec83ca
CalebeLAR <ca6ele@gmail.com>
2022-11-19 22:35:26 -0400

    test: req05 - adicionando mais 6 testes

--- src/tests/Wallet.test.js
+++ src/tests/Wallet.test.js
@@ -18,4 +18,15 @@ describe('testes para a página da wallet', () => {
 
     expect(history.location.pathname).toBe('/carteira');
   });
+  describe('testes pra verificar os inputs', () => {
+    test.todo('(2)deve ser rederizado um input do tipo text, envolto por uma label com o texto "Valor da despesa:"');
+    test.todo('(3)deve ser renderizado um input do tipo text, envolto por uma label com o texto "Descrição da despesa:"')
+    test.todo('(4)deve ser renderizado um select, envolto por uma label com o texto "moeda"');
+    test.todo('(5)deve ser renderizado um select, envolto por uma label com o texto "Método de pagamento"');
+    test.todo('(6)deve ser renderizado um select, envolto por uma label com o texto "Categoria para a despesa (tag)"');
+  });
+
+  describe('testes pra verificar os botões', () => {
+    test.todo('(7)deve ser rederizado um botão com o texto Adicionar Despesa"');
+  });
 });
